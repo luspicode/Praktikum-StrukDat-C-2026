9d8fe7b14769e7a69c213e9caa87b8f3d02ea001
luspicode <krishnaharyadiluthfi@gmail.com>
2026-04-30 10:07:15 +0700

    kumpul tugas

---
+++ pertemuan_12/binary_tree.py
@@ -0,0 +1,68 @@
+class TreeNode:
+  def __init__(self, data):
+    self.data = data
+    self.kanan = None
+    self.kiri = None
+    self.prev = None
+    
+
+root = TreeNode('A')
+root.kiri = TreeNode('B')
+root.kiri.prev = root
+root.kiri.kiri = TreeNode('D')
+root.kiri.kiri.prev = root.kiri
+root.kiri.kanan = TreeNode('E')
+root.kiri.kanan.prev = root.kiri
+root.kanan = TreeNode('C')
+root.kanan.prev = root
+root.kanan.kanan = TreeNode('F')
+root.kanan.kanan.prev = root.kanan
+
+
+def preOrderTraversal(root):
+  if root is None:
+    return
+  print(root.data, end=", ")
+  preOrderTraversal(root.kiri)
+  preOrderTraversal(root.kanan)
+
+def inOrderTraversal(root):
+  if root is None:
+    return
+  inOrderTraversal(root.kiri)
+  print(root.data, end=", ")
+  inOrderTraversal(root.kanan)
+
+def postOrderTraversal(root):
+  if root is None:
+    return
+  postOrderTraversal(root.kiri)
+  postOrderTraversal(root.kanan)
+  print(root.data, end=", ")
+
+def get_leaves(root):
+  if root is None:
+    return []
+  if not root.kiri and not root.kanan:
+    return [root.data]
+  return get_leaves(root.kiri) + get_leaves(root.kanan)
+
+# print('root :', root.data)
+# print('anak kiri dari A :', root.kiri.data)
+# print('anak kanan dari A :', root.kanan.data)
+# print('anak kiri dari B :', root.kiri.kiri.data)
+# print('anak kanan dari B :', root.kiri.kanan.data)
+# print('anak kanan dari C :', root.kanan.kanan.data)
+
+leaf = get_leaves(root)
+print(leaf)
+print('preOrderTranversalRoot ', end=': ')
+preOrderTraversal(root)
+print()
+print('inOrderTranversalRoot ', end=': ')
+inOrderTraversal(root)
+print()
+print('postOrderTranversalRoot ', end=': ')
+postOrderTraversal(root)
+print()
+
